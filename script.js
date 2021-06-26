8cd20ab16165b2c39572f52cc08eac4eeb00ccf5
dwinovic <nd.cahya19@gmail.com>
2021-06-26 22:51:16 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const changeColor = (id) => {
+  console.log(id);
+};
+// document.getElementsByClassName('object').addEventListener('click', testLog);
+
+const testLog = () => {
+  console.log('Connect');
+};
+
+const selectColor = (id) => {
+  console.log(id);
+};
