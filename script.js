3f3adb75b8255fa15ab5c835ee6bb11626efac77
dwinovic <nd.cahya19@gmail.com>
2021-06-27 09:30:53 +0700

    add modal

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let pickColor = '';
+let pickColor = [];
+let idItem = 0;
+
+// Data color
+const dataItem = [];
 
 const changeColor = (id) => {
   pickColor = id;
@@ -9,9 +13,31 @@ const testLog = () => {
   console.log('Connect');
 };
 
-const selectColor = (id) => {
-  const getNewColor = pickColor;
-  const newColor = document.getElementById(`${id}`);
-  newColor.className += ` ${getNewColor}`;
-  console.log(id);
+const selectItem = (id) => {
+  // const getNewColor = pickColor;
+  // const newColor = document.getElementById(`${id}`);
+  // newColor.className += ` ${getNewColor}`;
+  // console.log(id);
+  idItem = id;
+  console.log('Id Item: ', idItem);
+};
+
+// MODAL
+const onlyOne = (checkbox) => {
+  if (pickColor.length >= 2) {
+    pickColor = [];
+  }
+  const checkboxes = document.getElementsByName('check-color');
+  checkboxes.forEach((item) => {
+    if (item !== checkbox) {
+      // pickColor.pop();
+      item.checked = false;
+    }
+    if (item === checkbox) {
+      // const data = [item.id, item.alt];
+      pickColor.push(item.id, item.alt);
+    }
+  });
+
+  console.log('picked color: ', pickColor);
 };
